89e67f0682c75e2c131be827ad84aa2aa565373a
Nabeelep <84263179+Nabeelep@users.noreply.github.com>
2022-07-27 16:50:13 +0530

    added the user signup,bcrypt,session management

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,11 @@ const userHelpers = require('../helpers/user-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let user=req.session.user
+  console.log(user)
   productHelpers.getAllProducts().then((products)=>{
-    console.log(products)
-  res.render('user/view-products', { products ,admin:false}); })
+    // console.log(products)
+  res.render('user/view-products', { products,user}); })
 });
 router.get('/login',(req,res)=>{
   res.render('user/login')
@@ -20,6 +22,22 @@ router.post('/signup',(req,res)=>{
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response);
   })
+})
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    }
+    else{
+      res.redirect('/login')
+    }
+  })
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
 })
 
 module.exports = router;
